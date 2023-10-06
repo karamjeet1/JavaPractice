96a471bc0faca08f39a4abf83458f8c6e0f8afff
Karam <Karam@KVIRK>
2023-10-06 08:18:42 +0530

    commit 1.4.0

--- DuplicateChar.java
+++ DuplicateChar.java
@@ -1,9 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class DuplicateChar {
     public static void main(String[] args) {
         String str = "buzzzee";
-        printDuplicate(str);
+        // printDuplicate(str);
+
+        printDuplicate2(str);
+
     }
 
+    // BruthForce method
     public static void printDuplicate(String str) {
         StringBuilder dupliChar = new StringBuilder();
 
@@ -14,6 +21,25 @@ public class DuplicateChar {
             }
         }
         System.out.println(dupliChar.toString());
+    }
+
+    // Using HashMap method
+
+    public static void printDuplicate2(String str) {
+
+        Map<Character, Integer> count = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            if (count.containsKey(str.charAt(i)))
+                count.put(str.charAt(i), count.get(str.charAt(i)) + 1);
+            else
+                count.put(str.charAt(i), 1);
+
+        }
+
+        for (Map.Entry<Character, Integer> mapElement : count.entrySet()) { // iterating through the unordered map
+            if (mapElement.getValue() > 1) // if the count of characters is greater than 1 then duplicate found
+                System.out.println(mapElement.getKey() + ", count = " + mapElement.getValue());
+        }
     }
 
 }
